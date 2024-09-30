5a7e7d5f377f229628fbc05568eafc920e63e558
Douaa <douaachemnane@gmail.com>
2024-09-30 15:00:29 +0100

    my first commit

---
+++ src/main/java/GestEmployee/entities/Employee.java
@@ -0,0 +1,13 @@
+package GestEmployee.entities;
+
+public class Employee {
+
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String department;
+    private String position;
+
+}
